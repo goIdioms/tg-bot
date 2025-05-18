69d2840a790b58f9cc4fc212f79ea34503c7cc9f
goldioms <mharkunov@mail.ru>
2025-05-18 19:27:01 +0300

    chore(database): add migration files and update dependencies
    Add initial migration files for database schema management and update go.mod and go.sum to include required dependencies for database migrations. Remove unnecessary comment in postgres.go to clean up the code.

---
+++ pck/database/migrations/000001_tabel.down.sql
@@ -0,0 +1,15 @@
+
+-- Применить миграции:
+-- migrate -database "postgres://user:password@localhost:5432/mydb?sslmode=disable" -path migrations up
+
+-- Откатить последнюю миграцию:
+-- migrate -database "postgres://user:password@localhost:5432/mydb?sslmode=disable" -path migrations down
+
+-- Откатить все миграции:
+-- migrate -database "postgres://user:password@localhost:5432/mydb?sslmode=disable" -path migrations drop
+
+-- Перейти к конкретной версии миграции:
+-- migrate -database "postgres://user:password@localhost:5432/mydb?sslmode=disable" -path migrations goto 2
+
+-- Проверить статус миграций:
+-- migrate -database "postgres://user:password@localhost:5432/mydb?sslmode=disable" -path migrations version
--- pck/database/postgres.go
+++ pck/database/postgres.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 
-	_ "github.com/jackc/pgx/v5/stdlib" // Регистрация драйвера
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/joho/godotenv"
 )
 
